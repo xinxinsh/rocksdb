9555374000a3353f12dfd59a07f59237b3f24738
xinxin shu <xinxin.shu@intel.com>
2014-07-31 08:51:57 +0800

    remove GNU Make specific command
    Signed-off-by: xinxin shu <xinxin.shu@intel.com>

---
+++ util/build_version.cc
@@ -0,0 +1,5 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:444feb56e6a60af43bc2f977388186588baa5f0f";
+const char* rocksdb_build_git_datetime = "rocksdb_build_git_datetime:Thu Jul 31 08:50:08 CST 2014";
+const char* rocksdb_build_compile_date = __DATE__;
+const char* rocksdb_build_compile_time = __TIME__;
